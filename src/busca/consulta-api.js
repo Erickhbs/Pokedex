e3853e54ced2d96759bb0328ea900e1726064392
Phelyppe Álex <phelyppesilva@gmail.com>
2022-12-11 20:22:05 -0300

    consumindo a api

--- src/busca/consulta-api.js
+++ src/busca/consulta-api.js
@@ -1,32 +1,35 @@
 import { useEffect, useState } from "react";
 import React from 'react';
 
+
+
 const Busca = () => {
 
-    const url = 'https://pokeapi.co/api/v2/pokemon'
-
     const [data, setData] = useState([])
+    const url = 'https://rickandmortyapi.com/api/character'
 
-    const pegarPokemons = async () => {
+    const pegarPersonagens = async () => {
         const resposta = await fetch(url)
-        const pokemons = await resposta.json()
-        console.log(pokemons)
-
-        setData(pokemons.results)
+        const personagens = await resposta.json()
+        console.log(personagens.results)
+        setData(personagens.results)
     }
 
     useEffect( () => {
-        pegarPokemons()
-    }, [])
+        pegarPersonagens()
+    },[])
 
-    return(<>
-        <section>{data.map( (c) => {
-            return<>
-                <h2>{c.name}</h2>
-                <p>{c.url}</p>
-            </>
-        })}</section>
-    </>);
+    //<img src={getSource(key)}/>
+
+    return<section class="flex-container">{data.map( (p) => {
+        return<div class="card text-bg-secondary mb-3">
+            <img src={p.image} alt="personagem - {p.id}" class="card-img-top"/>
+            <div class="card-body">
+                <h5>{p.name.toUpperCase()}</h5>
+                <a href="https://www.freepnglogos.com/uploads/rick-and-morty-png/rick-and-morty-mind-bending-season-mysteries-quidd-6.png" class="btn btn-light">Mais sobre</a>
+            </div>
+        </div>
+    })}</section>
 }
 
 export default Busca;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Busca from './busca/consulta-api';
-
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
